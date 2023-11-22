f9c357172de127d5856c173f75fa8d679d489500
Lyulin25 <linlv1996@hotmail.com>
2023-11-22 10:08:02 +0800

    Initial Commit

---
+++ CustomerMerit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomerMerit
+//
+//  Created by Lyu Lin on 2023/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomerMerit/CustomerMeritApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomerMeritApp.swift
+//  CustomerMerit
+//
+//  Created by Lyu Lin on 2023/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomerMeritApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
